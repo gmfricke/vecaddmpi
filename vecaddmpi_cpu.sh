f5100e744594449cc672faa5c582307cc9abdd1b
Matthew Fricke <matthew@fricke.co.uk>
2023-08-25 02:22:12 -0600

    Fixed up CPU case SLURM script

--- vecaddmpi_cpu.sh
+++ vecaddmpi_cpu.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+#SBATCH --job-name=vecaddmpi
 #SBATCH --ntasks=4
+#SBATCH --time=00:10:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-user=yourusername@email.addr
+#SBATCH --mail-type=All
+#SBATCH --output=vecaddmpi_cpu.out
 
-module load openmpi/4.1.2-q2zi
+module load openmpi
+srun vecadd_mpi_cpu
 
-srun ./vecaddmpi
