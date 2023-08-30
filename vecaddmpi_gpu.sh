5b82b80d1bd42547a78c07519672f924d9d4c017
Matthew Fricke <matthew@fricke.co.uk>
2023-08-30 14:22:30 -0600

    Changed GPU resources to legal value

--- vecaddmpi_gpu.sh
+++ vecaddmpi_gpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=vecaddmpi_gpu
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=3
 #SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=4G
-#SBATCH --gpus 4
+#SBATCH --gpus 2
 #SBATCH --mail-user=yourusername@email.addr
 #SBATCH --mail-type=All
 #SBATCH --output=vecaddmpi_gpu.out
